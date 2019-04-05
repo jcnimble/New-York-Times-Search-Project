498412f8e4f5d0bc0a59c3adc11b192678418467
sharky <zeebing@gmail.com>
2019-04-05 13:59:28 -0400

    added function to build out link elements that contain the headline and byline

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,5 +13,32 @@ $(document).ready(function(){
     $("#clear-all").on("click", function(event){
         event.preventDefault();
         $("#well-section").empty();
-    })
+    });
+
+
+    function addArticle(index,headlineText,bylineText,articleURL){
+        var index = index + 1;
+        var link = $("<a>");
+        var article = $("<div>");
+        article.attr({"class":"well","id":"article-well-"+index});
+        
+        var headline = $("<h3>");
+        headline.attr("class","article-headline");
+
+        var badge = $("<span>");
+        badge.attr("class","label label-primary").text(index);
+
+        headline.append(badge).append("<strong>"+headlineText+"</strong>");
+        
+        var byline = $("<h5>");
+        byline.text(bylineText);
+
+        //add all to page
+        article.append(headline).append(byline);
+
+        link.attr("href",articleURL);
+        link.html(article);
+
+        $("#well-section").append(link);
+    }
 });
